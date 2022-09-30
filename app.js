6a62a8139ca5fdaacbb2d7c7af389534dd682f05
Gonzalo Arguello <garguello@bacoclub.com.ar>
2022-09-30 12:26:17 -0300

    agregados

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
+
 const express = require('express');
 const app = express();
+app.use(require('express-status-monitor')());
+
 const routes = require('./routes.js');
 
+app.use(require('express-status-monitor')());
 app.use('/',routes);
 
 module.exports = app;
